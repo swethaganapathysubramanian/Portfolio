e0416e242f3db8b0a0988bd442c25c0b466cce0b
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-08-05 13:04:32 -0700

    Updates

---
+++ precache-manifest.0c670be960d2976d3c9de64d3a9a0234.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b444ff1046757ae0c54954281c27f48",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "173b1036db6f4c54cc84",
+    "url": "/portfolio/static/css/main.34033955.chunk.css"
+  },
+  {
+    "revision": "533d91cc7c0adbf9cca0",
+    "url": "/portfolio/static/js/2.d0494977.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.d0494977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173b1036db6f4c54cc84",
+    "url": "/portfolio/static/js/main.545d8f1a.chunk.js"
+  },
+  {
+    "revision": "d993f6c8a4b72cbb0fd2",
+    "url": "/portfolio/static/js/runtime-main.48238cd8.js"
+  },
+  {
+    "revision": "1b941798754db9943aaa1b9981ec7b3c",
+    "url": "/portfolio/static/media/BankLandingPage.1b941798.PNG"
+  },
+  {
+    "revision": "c0509ca81279504b1b48a3e33a1b6db0",
+    "url": "/portfolio/static/media/Blogopedia.c0509ca8.PNG"
+  },
+  {
+    "revision": "25f491cc40aa929764e66e33327437e6",
+    "url": "/portfolio/static/media/Grocery Shopping.25f491cc.PNG"
+  },
+  {
+    "revision": "3a19cd21a57e67e5889676809d2c8b44",
+    "url": "/portfolio/static/media/Recipedetector.3a19cd21.PNG"
+  },
+  {
+    "revision": "fa9a32903a22a486025c329052cd4d1e",
+    "url": "/portfolio/static/media/hero.fa9a3290.jpg"
+  },
+  {
+    "revision": "111320eef0d60beb13b826f0a2976c1c",
+    "url": "/portfolio/static/media/myImage.111320ee.JPG"
+  }
+]);
